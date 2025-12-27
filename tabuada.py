847d9ef6a58d30a61f6258cd4db4502a87b4f0b7
Kayronbc <kayronrdm1@gmail.com>
2025-12-27 15:57:05 -0300

    Primeiros passos e projeto "Tabuada"

---
+++ tabuada.py
@@ -0,0 +1,20 @@
+#Exercício: Tabuada
+
+print("=== Tabuada ===")
+print("Digite um número para ver sua tabuada:")
+num = int(input("Número: "))
+
+print("Tabuada de", num)
+print("------------------")
+print(num, "x 1 =", num * 1)
+print(num, "x 2 =", num * 2)
+print(num, "x 3 =", num * 3)
+print(num, "x 4 =", num * 4)
+print(num, "x 5 =", num * 5)
+print(num, "x 6 =", num * 6)
+print(num, "x 7 =", num * 7)
+print(num, "x 8 =", num * 8)
+print(num, "x 9 =", num * 9)
+print(num, "x 10 =", num * 10)
+print("------------------")
+print("Fim da tabuada.")
